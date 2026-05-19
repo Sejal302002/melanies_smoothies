82e8f3c352c28dc6f6e77787bd9fbced751720a3
Sejal302002 <kumbharsejal24@gmail.com>
2026-05-19 12:19:12 +0530

    Modify dataframe to select additional column
    Updated the dataframe selection to include 'SEARCH_ON' column.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,12 @@ st.write(
 name_on_order = st.text_input("Name on smoothie:")
 st.write("The name of your smoothie will be:",name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table(
+    "smoothies.public.fruit_options"
+).select(
+    col('FRUIT_NAME'),
+    col('SEARCH_ON')
+)#st.dataframe(data=my_dataframe, use_container_width=True)
 
 
 ingredients_list = st.multiselect(
